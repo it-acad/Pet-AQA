885d8914970012c7a66bdd52bc9983c61342dbcd
Maxi-gorislav <maks.goryslav@gmail.com>
2023-04-02 21:11:55 +0300

    1

--- src/test/java/core/amadeus/AmadeusTests.java
+++ src/test/java/core/amadeus/AmadeusTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 @DisplayName("[Base Amadeus test]")
 public class AmadeusTests extends BaseApiTests {
     @Test
-    @Disabled
     @DisplayName("Verify flight destination")
     public void verifyAddAmadeus() {
         new AmadeusController()
