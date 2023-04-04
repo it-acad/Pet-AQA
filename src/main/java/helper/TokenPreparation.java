9b8ff373805c7b12b2ad00a500d45a49a18e84d0
Maxi-gorislav <maks.goryslav@gmail.com>
2023-04-04 14:04:44 +0300

    add some step and small refactor

--- src/main/java/helper/TokenPreparation.java
+++ src/main/java/helper/TokenPreparation.java
@@ -2,6 +2,8 @@ package helper;
 
 import config.AmadeusToken;
 import config.Conf;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
@@ -17,7 +19,11 @@ public class TokenPreparation {
                 .formParam("grant_type", "client_credentials")
                 .post("/v1/security/oauth2/token").jsonPath();
 
-        return new AmadeusToken(result.getString("access_token"),
-                result.getLong("expires_in"), result.prettyPrint());
+        String accessToken = result.getString("access_token");
+        long expiresIn = result.getLong("expires_in");
+
+        Allure.step("Generated amadeus token: " + accessToken);
+
+        return new AmadeusToken(accessToken, expiresIn, result.prettyPrint());
     }
 }
--- src/main/java/helper/clean_up/CleanUpExtension.java
+++ src/main/java/helper/clean_up/CleanUpExtension.java
@@ -1,5 +1,6 @@
 package helper.clean_up;
 
+import io.qameta.allure.Allure;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import java.util.Set;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CleanUpExtension implements AfterAllCallback {
 
@@ -19,15 +21,20 @@ public class CleanUpExtension implements AfterAllCallback {
     @Override
     public void afterAll(ExtensionContext context) {
         for (Long id : idsToDelete) {
-            given()
-                    .log().all()
-                    .baseUri("https://petstore3.swagger.io")
-                    .basePath("/api/v3/")
-                    .accept(ContentType.JSON)
-                    .contentType(ContentType.JSON)
-                    .delete("/pet/{id}", id)
-                    .then()
-                    .statusCode(200);
+            Allure.step("Delete pet with ID: " + id + " and verify status code is 200", () -> {
+                int statusCode = given()
+                        .log().all()
+                        .baseUri("https://petstore3.swagger.io")
+                        .basePath("/api/v3/")
+                        .accept(ContentType.JSON)
+                        .contentType(ContentType.JSON)
+                        .delete("/pet/{id}", id)
+                        .then()
+                        .extract()
+                        .statusCode();
+
+                assertEquals(200, statusCode, "Status code is not 200 but {statusCode}");
+            });
         }
     }
 }
